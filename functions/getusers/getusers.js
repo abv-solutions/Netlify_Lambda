7c74adc120d6a80c1d900813e6a8c58f4a7ea82c
Andrei Suciu <andreisuciu2005@yahoo.com>
2020-02-18 09:30:04 +0200

    change

--- functions/getusers/getusers.js
+++ functions/getusers/getusers.js
@@ -2,11 +2,15 @@ const axios = require('axios');
 
 const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
 
-const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
-
 exports.handler = async (event, context) => {
   if (event.httpMethod == 'GET') {
-    return axios.get(URL)
+    return axios
+      .get(API_URL, {
+        auth: {
+          username: API_CLIENT_ID,
+          password: API_CLIENT_SECRET
+        }
+      })
       .then(res => ({
         statusCode: 200,
         body: JSON.stringify(res.data)
@@ -16,4 +20,4 @@ exports.handler = async (event, context) => {
         body: err.toString()
       }));
   }
-}
+};
